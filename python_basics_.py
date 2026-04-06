0154c1cc4e49cfc64ce455de85671dee2030f189
chintu2004-cpu <nainisridhar1592@gmail.com>
2026-04-06 12:33:59 +0530

    Add files via upload

---
+++ python_basics_.py
@@ -0,0 +1,33 @@
+name="sridhar"
+
+age=22
+
+height=5.9
+
+is_student=true
+
+print(name,age,height,is_student)
+
+
+
+#string formating
+
+print(f"name:{name} | age:{age} | height:{height} | student:{is _student}")
+
+
+
+age_in_months=age*12
+
+age_in_days=age*365
+
+age_mod_7=%7
+
+age_power_2=age**
+
+print("age in months:", age_in_months)
+
+print("age in days:",age_in_days)
+
+print("the reminder when age is divided:"age_mod_7)
+
+print("age is squared :"age_power_2)
